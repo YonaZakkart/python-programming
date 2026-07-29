f7d569e9e8545597883561c7947730f05d8f7f16
YonaZakkart <musicyonathan.com>
2026-07-28 20:45:02 -0600

    Adding Class 04 --temp

--- Class04/temp.py
+++ Class04/temp.py
@@ -24,5 +24,3 @@ print("Hola mi nombre es")
 # print("Hola!! \nSoy estudiante de Software \nEn la materia Estructura de datos")
 
 print("Nombre Nota1 Nota2 Nota3 \n Ana \t10 \t10 \t10 \n Josué \t9 \t8 \t7")
-
-# borrar
