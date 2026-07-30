49e1223495381d462cca6619799560229a0b965f
YonaZakkart <musicyonathan.com>
2026-07-30 15:25:56 -0600

    Updating Class 03 --print

--- Class03/01-print.py
+++ Class03/01-print.py
@@ -1,9 +1,13 @@
+# Reasignacion de variables y operaciones numéricas 
+
 x = 10
 y =10.5
-x = 5
+x = 5 # Sobrescribimos el valor previo de x de 10 a 5
 print(x * y)
 
+# cadenas de texto (Strings)
 nombre = "Yonathan"
 print(nombre)
 
-print(len(float))
+# La instrucción 'len(float)' generara un TypeError 
+# debido a que 'len()' espera una secuenci como un str o list y no un tipo de dato directo
