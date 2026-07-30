6bf9b1ce13eb076c868abe10349ccd5df2e397b2
YonaZakkart <musicyonathan.com>
2026-07-30 15:27:20 -0600

    Updating Class 05 --numeros

--- Class05/numeros.py
+++ Class05/numeros.py
@@ -1,14 +1,18 @@
-# operadores matematicas
+# Operadores aritmeticos
 n1 = 5
 n2 = 10
 n3 = 15
 n4 = 20
 
+# Conversion a cadena (str) para concatenar con texto
 print("La suma de n1 + n2 es: " + str(n1+n2))
 print("La resta de n4 - n3 es: " + str(n4-n3))
 print("El resutado de la multiplicacion de n2 y n3 es: " + str(n2*n3))
 print("El resutado de la division de n1 y n4 es: " + str(n1/n4))
 
-# Elevar a la X potencia
+# Operaciones Potencia
+# Operador '**' eleva un número
 print("La elevacion del numero n1 a n2 es: " + str(n1**n2))
+# Operador % devuelve el residuo de la división. 
+# Si residuo == 0 es par y si es 1 es impar.
 print("El numero " + str(n2) + " es par o impar?: " + str(n3 % 2))
