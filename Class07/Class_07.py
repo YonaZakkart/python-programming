9f2905f9efd4829c5a47d1b7ba880a37d2e098fe
YonaZakkart <musicyonathan@gmail.com>
2026-08-16 18:28:30 -0600

    Updating Class 07

---
+++ Class07/Class_07.py
@@ -0,0 +1,16 @@
+nombre = "Yonathan"
+print(nombre)
+print(nombre[3])
+print(nombre[-1])
+
+
+usuario = "YonaZakkart"
+nuevo_usuario = "C" + usuario[1:]
+print(nuevo_usuario)
+
+producto = "Cuadernos"
+print(producto[6:])
+print(producto[2:6])
+print(producto[:3])
+
+print(producto[::2])
